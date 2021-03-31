15e9e968b9fa6608d567266918792f24057c531e
gargamel24 <81331003+gargamel24@users.noreply.github.com>
2021-03-31 17:34:34 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,57 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.setPosition(randint(20, 140), randint(10, 110))
+})
 let donut = sprites.create(img`
-    . . . . . . b b b b a a . . . . 
-    . . . . b b d d d 3 3 3 a a . . 
-    . . . b d d d 3 3 3 3 3 3 a a . 
-    . . b d d 3 3 3 3 3 3 3 3 3 a . 
-    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
-    . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
-    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
-    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
-    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
-    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
-    a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
-    a a 3 3 3 d d d a a 4 4 4 e e . 
-    . e a a a a a a 4 4 4 4 e e . . 
-    . . e e b b 4 4 4 4 b e e . . . 
-    . . . e e e e e e e e . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, 0)
-controller.moveSprite(donut, 200, 200)
+    ..............ffffff....
+    .............f2feeeeff..
+    ............f222feeeeff.
+    .......cc...feeeeffeeef.
+    .......cdc.fe2222eeffff.
+    .......cddcf2effff222ef.
+    ........cddcffeeefffffff
+    .........cddce44fbe44eff
+    ..........cdceddf14d4eef
+    ..........cccdeddd4eeef.
+    ...........edd4e44eeff..
+    ............ee442222f...
+    .............f2e2222f...
+    .............f554444f...
+    ..............ffffff....
+    ................fff.....
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+controller.moveSprite(donut, 300, 300)
 donut.setStayInScreen(true)
-scene.setBackgroundColor(6)
+scene.setBackgroundColor(9)
+let pearl = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Food)
+pearl.setPosition(randint(20, 140), randint(10, 110))
+game.onUpdate(function () {
+	if (info.score()== 5){
+        game.over(true)
+    }                         
+})
